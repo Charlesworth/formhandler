7ee0969974700c571f0e2f00d0a497d9cce69827
charlesworth <cochrane26@gmail.com>
2020-09-16 17:48:05 +0100

    add tests for url encoded forms

--- main_test.go
+++ main_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
+	"mime/multipart"
 	"net/http"
-	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
@@ -12,76 +10,84 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHandleSimpleForm(t *testing.T) {
-	req, err := http.NewRequest(http.MethodPost, "/", nil)
-	if err != nil {
-		t.Fatal(err)
+// func TestHandleSimpleForm(t *testing.T) {
+// 	req, err := http.NewRequest(http.MethodPost, "/", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	rr := httptest.NewRecorder()
+// 	handler := http.HandlerFunc(handleSimpleForm)
+
+// 	handler.ServeHTTP(rr, req)
+
+// 	assert.Equal(t, http.StatusOK, rr.Code, "handler expected success for basically anything!")
+// }
+
+// func TestHandleSimpleFormWithValidation(t *testing.T) {
+// 	req, err := http.NewRequest(http.MethodPost, "/", nil)
+// 	assert.NoError(t, err)
+
+// 	rr := httptest.NewRecorder()
+// 	handler := http.HandlerFunc(handleFormWithMultiFile)
+
+// 	handler.ServeHTTP(rr, req)
+
+// 	assert.Equal(t, http.StatusBadRequest, rr.Code, "expected an error for no request body")
+// }
+
+func TestGetFormContent(t *testing.T) {
+	var formContentTests = []struct {
+		testName               string
+		testRequestConstructor func() (*http.Request, error)
+		expectedResultOutput   map[string][]string
+		expectedFileOutput     map[string][]*multipart.FileHeader
+	}{
+		{"url encoded form with no fields", func() (*http.Request, error) {
+			form := url.Values{}
+			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			return r, err
+		}, map[string][]string{}, nil},
+
+		{"url encoded form with only empty fields", func() (*http.Request, error) {
+			form := url.Values{"field1": {}, "field2": {}}
+			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			return r, err
+		}, map[string][]string{}, nil},
+
+		{"url encoded form with some filled and some empty fields", func() (*http.Request, error) {
+			form := url.Values{"field1": {"value1"}, "field2": {}}
+			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			return r, err
+		}, map[string][]string{"field1": {"value1"}}, nil},
+
+		{"url encoded form with no empty fields", func() (*http.Request, error) {
+			form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
+			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			return r, err
+		}, map[string][]string{"field1": {"value1"}, "field2": {"value2"}}, nil},
+
+		{"multipart form with no empty fields", func() (*http.Request, error) {
+			form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
+			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			return r, err
+		}, map[string][]string{"field1": {"value1"}, "field2": {"value2"}}, nil},
 	}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleSimpleForm)
+	for _, tt := range formContentTests {
+		t.Run(tt.testName, func(t *testing.T) {
+			r, err := tt.testRequestConstructor()
+			assert.NoError(t, err)
 
-	handler.ServeHTTP(rr, req)
+			results, files, err := getFormContent(r)
 
-	assert.Equal(t, http.StatusOK, rr.Code, "handler expected success for basically anything!")
-}
-
-func TestHandleSimpleFormWithValidation(t *testing.T) {
-	req, err := http.NewRequest(http.MethodPost, "/", nil)
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleFormWithMultiFile)
-
-	handler.ServeHTTP(rr, req)
-
-	assert.Equal(t, http.StatusBadRequest, rr.Code, "expected an error for no request body")
-}
-
-func TestWhatever(t *testing.T) {
-	// To test a "application/x-www-form-urlencoded" form, encode the form results as url.Values
-	form := url.Values{"key": {"Value"}, "id": {"123"}}
-	req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleSimpleForm)
-	handler.ServeHTTP(rr, req)
-	assert.Equal(t, http.StatusBadRequest, rr.Code, "expected an error for no request body")
-}
-
-func TestWhatever1(t *testing.T) {
-	// To test a "application/x-www-form-urlencoded" form, encode the form results as url.Values
-	form := url.Values{"email": {"Value"}, "subject": {"asdf"}, "message": {"123"}}
-	req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleSimpleForm)
-	handler.ServeHTTP(rr, req)
-	assert.Equal(t, http.StatusOK, rr.Code, "expected an error for no request body")
-	bodyBytes, err := ioutil.ReadAll(rr.Body)
-	if err != nil {
-		log.Fatal(err)
+			assert.Equal(t, tt.expectedResultOutput, results, "unexpected parsed form results")
+			assert.Equal(t, tt.expectedFileOutput, files, "unexpected form files")
+		})
 	}
-	assert.Empty(t, string(bodyBytes))
-}
-
-func TestWhatever2(t *testing.T) {
-	// To test a "application/x-www-form-urlencoded" form, encode the form results as url.Values
-	form := url.Values{"subject": {"asdf"}, "message": {"123"}}
-	req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handleSimpleForm)
-	handler.ServeHTTP(rr, req)
-	assert.Equal(t, http.StatusBadRequest, rr.Code, "expected an error for no request body")
-	bodyBytes, err := ioutil.ReadAll(rr.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	assert.Contains(t, string(bodyBytes), "email")
 }
