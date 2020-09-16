44eb22d34195466a2fadbe698e485cea82e49b5f
charlesworth <cochrane26@gmail.com>
2020-09-16 17:55:30 +0100

    refactor the url encoded forms test

--- main_test.go
+++ main_test.go
@@ -10,31 +10,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestHandleSimpleForm(t *testing.T) {
-// 	req, err := http.NewRequest(http.MethodPost, "/", nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(handleSimpleForm)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	assert.Equal(t, http.StatusOK, rr.Code, "handler expected success for basically anything!")
-// }
-
-// func TestHandleSimpleFormWithValidation(t *testing.T) {
-// 	req, err := http.NewRequest(http.MethodPost, "/", nil)
-// 	assert.NoError(t, err)
-
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(handleFormWithMultiFile)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	assert.Equal(t, http.StatusBadRequest, rr.Code, "expected an error for no request body")
-// }
+func constructURLEncodedForm(values url.Values) (*http.Request, error) {
+	r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(values.Encode()))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return r, err
+}
 
 func TestGetFormContent(t *testing.T) {
 	var formContentTests = []struct {
@@ -43,40 +23,49 @@ func TestGetFormContent(t *testing.T) {
 		expectedResultOutput   map[string][]string
 		expectedFileOutput     map[string][]*multipart.FileHeader
 	}{
-		{"url encoded form with no fields", func() (*http.Request, error) {
-			form := url.Values{}
-			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			return r, err
-		}, map[string][]string{}, nil},
-
-		{"url encoded form with only empty fields", func() (*http.Request, error) {
-			form := url.Values{"field1": {}, "field2": {}}
-			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			return r, err
-		}, map[string][]string{}, nil},
-
-		{"url encoded form with some filled and some empty fields", func() (*http.Request, error) {
-			form := url.Values{"field1": {"value1"}, "field2": {}}
-			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			return r, err
-		}, map[string][]string{"field1": {"value1"}}, nil},
-
-		{"url encoded form with no empty fields", func() (*http.Request, error) {
-			form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
-			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			return r, err
-		}, map[string][]string{"field1": {"value1"}, "field2": {"value2"}}, nil},
-
-		{"multipart form with no empty fields", func() (*http.Request, error) {
-			form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
-			r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-			return r, err
-		}, map[string][]string{"field1": {"value1"}, "field2": {"value2"}}, nil},
+		{
+			"url encoded form with no fields",
+			func() (*http.Request, error) {
+				return constructURLEncodedForm(url.Values{})
+			},
+			map[string][]string{},
+			nil,
+		},
+		{
+			"url encoded form with only empty fields",
+			func() (*http.Request, error) {
+				return constructURLEncodedForm(url.Values{"field1": {}, "field2": {}})
+			},
+			map[string][]string{},
+			nil,
+		},
+		{
+			"url encoded form with some filled and some empty fields",
+			func() (*http.Request, error) {
+				return constructURLEncodedForm(url.Values{"field1": {"value1"}, "field2": {}})
+			},
+			map[string][]string{"field1": {"value1"}},
+			nil,
+		},
+		{
+			"url encoded form with no empty fields",
+			func() (*http.Request, error) {
+				return constructURLEncodedForm(url.Values{"field1": {"value1"}, "field2": {"value2"}})
+			},
+			map[string][]string{"field1": {"value1"}, "field2": {"value2"}},
+			nil,
+		},
+		// {
+		// 	"multipart form with no empty fields",
+		// 	func() (*http.Request, error) {
+		// 		form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
+		// 		r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
+		// 		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		// 		return r, err
+		// 	},
+		// 	map[string][]string{"field1": {"value1"}, "field2": {"value2"}},
+		// 	nil,
+		// },
 	}
 
 	for _, tt := range formContentTests {
