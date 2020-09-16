24fbca7520598fa330fb736f2fe33a2d99633b54
charlesworth <cochrane26@gmail.com>
2020-09-16 18:22:54 +0100

    added multipart form tests

--- main_test.go
+++ main_test.go
@@ -1,82 +1,151 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func constructURLEncodedForm(values url.Values) (*http.Request, error) {
+func constructURLEncodedForm(values url.Values) (*http.Request, func(), error) {
 	r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(values.Encode()))
 	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	return r, err
+	return r, func() {}, err
 }
 
 func TestGetFormContent(t *testing.T) {
 	var formContentTests = []struct {
 		testName               string
-		testRequestConstructor func() (*http.Request, error)
+		testRequestConstructor func() (req *http.Request, cleanupFunc func(), err error)
 		expectedResultOutput   map[string][]string
-		expectedFileOutput     map[string][]*multipart.FileHeader
+		expectedFiles          int
 	}{
 		{
 			"url encoded form with no fields",
-			func() (*http.Request, error) {
+			func() (*http.Request, func(), error) {
 				return constructURLEncodedForm(url.Values{})
 			},
 			map[string][]string{},
-			nil,
+			0,
 		},
 		{
 			"url encoded form with only empty fields",
-			func() (*http.Request, error) {
+			func() (*http.Request, func(), error) {
 				return constructURLEncodedForm(url.Values{"field1": {}, "field2": {}})
 			},
 			map[string][]string{},
-			nil,
+			0,
 		},
 		{
 			"url encoded form with some filled and some empty fields",
-			func() (*http.Request, error) {
+			func() (*http.Request, func(), error) {
 				return constructURLEncodedForm(url.Values{"field1": {"value1"}, "field2": {}})
 			},
 			map[string][]string{"field1": {"value1"}},
-			nil,
+			0,
 		},
 		{
 			"url encoded form with no empty fields",
-			func() (*http.Request, error) {
+			func() (*http.Request, func(), error) {
 				return constructURLEncodedForm(url.Values{"field1": {"value1"}, "field2": {"value2"}})
 			},
 			map[string][]string{"field1": {"value1"}, "field2": {"value2"}},
-			nil,
+			0,
 		},
-		// {
-		// 	"multipart form with no empty fields",
-		// 	func() (*http.Request, error) {
-		// 		form := url.Values{"field1": {"value1"}, "field2": {"value2"}}
-		// 		r, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(form.Encode()))
-		// 		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		// 		return r, err
-		// 	},
-		// 	map[string][]string{"field1": {"value1"}, "field2": {"value2"}},
-		// 	nil,
-		// },
+		{
+			"multipart form with no empty fields",
+			func() (*http.Request, func(), error) {
+				testFile1, testFile1CleanUp, err := tempTestFile("png")
+				values := map[string]io.Reader{
+					"file1":  testFile1,
+					"field1": strings.NewReader("value1"),
+				}
+				req, err := constructMultipartForm(values)
+
+				return req, testFile1CleanUp, err
+			},
+			map[string][]string{"field1": {"value1"}},
+			1,
+		},
 	}
 
 	for _, tt := range formContentTests {
 		t.Run(tt.testName, func(t *testing.T) {
-			r, err := tt.testRequestConstructor()
+			r, cleanup, err := tt.testRequestConstructor()
 			assert.NoError(t, err)
 
 			results, files, err := getFormContent(r)
 
 			assert.Equal(t, tt.expectedResultOutput, results, "unexpected parsed form results")
-			assert.Equal(t, tt.expectedFileOutput, files, "unexpected form files")
+			// TODO: this doesn't account for "multiple" attribute files
+			assert.Equal(t, tt.expectedFiles, len(files), "unexpected form files")
+			cleanup()
 		})
 	}
+}
+
+func tempTestFile(fileSuffix string) (file *os.File, cleanupFunc func(), err error) {
+	file, err = ioutil.TempFile(os.TempDir(), "testFile-*."+fileSuffix)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Write to the file
+	text := []byte("Writing some text into the temp test file")
+	if _, err = file.Write(text); err != nil {
+		return nil, nil, err
+	}
+
+	// // Close the file
+	// if err := file.Close(); err != nil {
+	// 	return nil, nil, err
+	// }
+
+	return file, func() { os.Remove(file.Name()) }, nil
+}
+
+func constructMultipartForm(values map[string]io.Reader) (r *http.Request, err error) {
+	// Prepare a form that you will submit to that URL.
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	for key, r := range values {
+		var fw io.Writer
+		if x, ok := r.(io.Closer); ok {
+			defer x.Close()
+		}
+		// Add an file
+		if x, ok := r.(*os.File); ok {
+			if fw, err = w.CreateFormFile(key, x.Name()); err != nil {
+				return nil, err
+			}
+		} else {
+			// Add other fields
+			if fw, err = w.CreateFormField(key); err != nil {
+				return nil, err
+			}
+		}
+		if _, err = io.Copy(fw, r); err != nil {
+			return nil, err
+		}
+	}
+
+	// Don't forget to close the multipart writer.
+	// If you don't close it, your request will be missing the terminating boundary.
+	w.Close()
+
+	// Now that you have a form, you can submit it to your handler.
+	req, err := http.NewRequest(http.MethodPost, "/", &b)
+	if err != nil {
+		return nil, err
+	}
+	// Don't forget to set the content type, this will contain the boundary.
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	return req, err
 }
